5f2771b3488c9b129dc2743814b8e4ecfa1e363e
Adam Zavadil <adamzav33@seznam.cz>
2025-05-07 22:35:20 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
-import Image from "next/image";
-import Header from "@/components/Header";
-import Services from "@/components/Services";
-import Recenze from "@/components/Recenze";
-import About from "@/components/About";
-import Resend from "@/components/Resend";
+import { Metadata } from 'next';
+import Header from '@/components/Header';
+import Services from '@/components/Services';
+import Recenze from '@/components/Recenze';
+import About from '@/components/About';
+import Resend from '@/components/Resend';
+
+export const metadata: Metadata = {
+  title: 'Detailing Matyáš Lexmaul',
+  description:
+    'Profesionální detailing vozidel v Drahanovicích – Matyáš Lexmaul nabízí špičkové čištění, leštění a ochranu laku v Olomouckém kraji. Objednejte se ještě dnes!',
+  keywords: [
+    'detailing Drahanovice',
+    'čištění aut Olomouc',
+    'leštění aut',
+    'Matyáš Lexmaul',
+    'ochrana laku',
+    'detailing Olomoucký kraj',
+  ],
+  authors: [{ name: 'Adam Zavadil' }],
+  openGraph: {
+    title: 'Detailing Matyáš Lexmaul',
+    description:
+      'Profesionální detailing a péče o auto v Drahanovicích. Kvalita, na kterou se můžete spolehnout.',
+    url: 'https://www.vase-domena.cz/',
+    type: 'website',
+  },
+};
 
 export default function Home() {
   return (
